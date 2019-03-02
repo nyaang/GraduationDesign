f10ce68da5022a56264d1ca84d2decd10192eda1
Yang <pccettfly@outlook.com>
2019-03-02 16:52:45 +0800

    update

--- crawl.py
+++ crawl.py
@@ -1,80 +1,58 @@
-import requests, re,  json, random, pymongo, threading,math,time,json
-# from lxml import etree
-# from time import localtime
-userids = json.load(open("userids.json", 'r'))
-
-
-
-class wbrelation(threading.Thread):
+import requests,pymongo,threading,time,json,re,random,math
+userids = json.load(open("userids.json",'r'))
+class NongGuanJia(threading.Thread):
     def __init__(self, userids):
         threading.Thread.__init__(self)
         self.userids = userids
         self.knum=0
-
     def run(self):
         for userid in self.userids:
             self.userid = userid
             self.getfollow(self.userid)
-
-
-
     def getrequest(self, url):
-       # proxie = json.load(open("proxies.json", 'r'))
-
+        #proxie = json.load(open("proxies.json", 'r'))
         try:
-
-          #  proxy = random.choice(proxie)
-           # r = requests.get(url, timeout=10, proxies=proxy)
-            r = requests.get(url, timeout=10)
+            #proxy = random.choice(proxie)
+            #r = requests.get(url, timeout=10, proxies=proxy)
+            r = requests.get(url, timeout=3)
             print("requested from:" + url)
-            #print (r)
             return r
         except requests.exceptions.ReadTimeout:
             print("requests.exceptions.ReadTimeout")
+            time.sleep(1)
             r = self.getrequest(url)
             return r
         except requests.exceptions.ConnectionError:
+            print("requests.exceptions.ConnectionError")
+            time.sleep(1)
             r = self.getrequest(url)
             return r
-        except :
+        except:
+            print("error")
+            time.sleep(1)
             r = self.getrequest(url)
             return r
-
-
-
-
-    def decodejson( self,r,url):
+    def decodejson(self,r,url):
         i=0
         try:
-
           data = r.json()
-
         except json.decoder.JSONDecodeError:
             i=i+1
-            if(i<10):
-
-
+            if(i<10):   #允许10次JSONDecodeError
               time.sleep(3)
-              self.decodejson( r, url)
+              self.decodejson(r, url)
               return data
-
-
-        except Exception as  e:
-            print("e :")
-            print (e)
+        except Exception as e:
+            print("Error:",e)
             r = self.getrequest(url)
             data = r.json()
             return data
         return data
 
-
-# 48\
-
     def getfollow(self,userid):
-
-        nowtime = '2018-09-10T00:00:00'
-        self.rcount = 0
-        self.qcount = 0
+        nowtime = '2019-03-01T00:00:00' #最新的抓取时间
+        self.rcount = 0 #回复数
+        self.qcount = 0 #问题数
         self.User = []
         self.Reply = []
         self.Question = []
@@ -84,27 +62,27 @@ class wbrelation(threading.Thread):
           url = 'http://ngjv4.laodao.so//ashx/news/sys_FriendNews.ashx?action=find4user&UserID=%s&lasttime=%s&pckey=&version=pc' % (self.userid,nowtime)
           r = self.getrequest(url)
           data =self.decodejson(r,url)
-          data1 = data["datas"]
-          code=data["code"]
+          datas = data["datas"]
+          code=data["code"] #响应码
           print(code)
-          if (len(data1) == 20):
-                nowtime = data1[-1]["UpdateDate"]
+          if (len(datas) == 20):
+                nowtime = datas[-1]["UpdateDate"]
           else:
-                nowtime = 'none'
-          if (code ==200 and len(data1)>0):
+                nowtime = 'none'    #当前页以为最末页
+          if (code ==200 and len(datas)>0):
             print("ok")
-            for i in range(0, len(data1)):
-                qid = data1[i]['CardID']
-                cropid = data1[i]["CropID"]
-                self.uid = data1[i]['UserID']
-                abs = data1[i]['abs']
-                aorq = data1[i]['type']
-                self.identities = data1[i]['identities']
-                self.pro = data1[i]['Province']
-                recount = data1[i]['ReplyCount']
-                time = data1[i]["UpdateDate"]
+            for i in range(0, len(datas)):
+                qid = datas[i]['CardID']    #问题Id
+                cropid = datas[i]["CropID"] #植物Id
+                abs = datas[i]['abs']   #问题文字摘要
+                aorq = datas[i]['type'] #值为0，表示提问；值为3，表示回答
+                self.identities = datas[i]['identities']
+                self.pro = datas[i]['Province']
+                self.city=datas[i]['City']
+                recount = datas[i]['ReplyCount'] #回答数
+                time = datas[i]["UpdateDate"]
 
-                if (aorq == 3):
+                if (aorq == 3): #抓回答
                     self.rcount += 1
                     self.Reply.append({"qid": qid,
                                        "cropid":cropid,
@@ -113,7 +91,7 @@ class wbrelation(threading.Thread):
                                   "time": time})
                     crop.add(cropid)
 
-                else:
+                else: #抓问题
                     print(qid)
                     self.qcount += 1
                     if (int(recount) <= 20):
@@ -122,34 +100,32 @@ class wbrelation(threading.Thread):
                         rr = self.getrequest(rurl)
                         rdata = self.decodejson(rr,rurl)
                         rcode = rdata["code"]
-
                         if (rcode == 200):
-                           rdata1 = rdata["datas"]
-
+                           rdatas = rdata["datas"]
                            qreply = []
-
-                           for j in range(0, len(rdata1)):
-                            content = rdata1[j]["contents"]
-                            ruid = rdata1[j]["UserID"]
-
-                            ridentities = rdata1[j]['identities']
-
-                            rtime = rdata1[j]["CreateTime"]
-                            pro = rdata1[j]['Province']
-                            zan = rdata1[j]["zan"]
-                            cai = rdata1[j]["cai"]
+                           for j in range(0, len(rdatas)):
+                            content = rdatas[j]["contents"] #回复的内容
+                            ruid = rdatas[j]["UserID"]
+                            ridentities = rdatas[j]['identities'] #值皆为2，为3时是提问者显示其他
+                            rtime = rdatas[j]["CreateTime"] #回复时间
+                            pro = rdatas[j]['Province']
+                            city= rdatas[j]['City']
+                            zan = rdatas[j]["zan"] #赞
+                            cai = rdatas[j]["cai"] #踩
                             qreply.append({"content": content,
                                            "ruid": ruid,
                                            "ridentities": ridentities,
                                            "province": pro,
+                                           "city":city,
                                            "time": rtime,
                                            "zan": zan,
                                            "cai": cai})
                         else:
                             qreply=[]
+                            print("问题网页响应码不为200，为"+str(rcode))
                     else:
-
-                        k = int(recount) // 20 + 1
+                        k = int(recount) // 20 + 1  #单页的reply数不是20，待查证大回复数问题的有效性，TODO
+                        print(recount)
                         for m in range(1, k+1):
                             rurl = 'http://ngjv4.laodao.so/ASHX/bbs_card.ashx?action=replylist&version=pc&ID=%s&pagSize=20&pagindex=%s' % (
                                 qid, m)
@@ -158,20 +134,17 @@ class wbrelation(threading.Thread):
                             rcode = rdata["code"]
 
                             if (rcode == 200):
-                             rdata1 = rdata["datas"]
-
+                             rdatas = rdata["datas"]
                              qreply = []
 
-                             for j in range(0, len(rdata1)):
-                                content = rdata1[j]["contents"]
-                                ruid = rdata1[j]["UserID"]
-
-                                ridentities = rdata1[j]['identities']
-
-                                rtime = rdata1[j]["CreateTime"]
-                                pro = rdata1[j]['Province']
-                                zan = rdata1[j]["zan"]
-                                cai = rdata1[j]["cai"]
+                             for j in range(0, len(rdatas)):
+                                content = rdatas[j]["contents"]
+                                ruid = rdatas[j]["UserID"]
+                                ridentities = rdatas[j]['identities']
+                                rtime = rdatas[j]["CreateTime"]
+                                pro = rdatas[j]['Province']
+                                zan = rdatas[j]["zan"]
+                                cai = rdatas[j]["cai"]
                                 qreply.append({"content": content,
                                                "ruid": ruid,
                                                "ridentities": ridentities,
@@ -181,6 +154,7 @@ class wbrelation(threading.Thread):
                                                "cai": cai})
                             else:
                                 qreply=[]
+                                print("问题网页响应码不为200，为" + str(rcode))
                     self.Question.append({"qid": qid,
                                           "cropid":cropid,
                                      "question": abs,
@@ -188,34 +162,31 @@ class wbrelation(threading.Thread):
                                      "time": time,
                                      "reply": qreply})
 
-        if(len(data1)>0):
+        if(len(datas)>0):   #筛去没有任何动态（问题或回答）的用户
            self.User.append({"uid": self.userid,
                     "identities": self.identities,
-                     "rcount": self.rcount,
-                     "qcount": self.qcount,
-
-                   "province": self.pro,
-                   "crops": list(crop)
+                    "rcount": self.rcount,
+                    "qcount": self.qcount,
+                    "province": self.pro,
+                    "city":self.city,
+                    "crops": list(crop)
             })
 
         if(len(self.User)>0):
           client = pymongo.MongoClient('127.0.0.1:27017')
           db = client['Nongguanjia']
-          db['nongguanjianew'].insert(
-          {"User": self.User,
-                            "Question":self.Question,
-                           "Reply":self.Reply})
+          db['nongguanjianew'].insert_one(
+            {"User": self.User,
+            "Question":self.Question,
+            "Reply":self.Reply})
 
         userids.remove(self.userid)
-        self.updateuserids(self.userid)
+        self.updateuserids()
 
-
-
-    def updateuserids(self,userid):
+    def updateuserids(self):
       userfile = open('usersnew.json', 'w')
       json.dump(userids, userfile, indent=4, sort_keys=False, ensure_ascii=False)
       userfile.close()
-
 
 def start(threadnum):
     linksqueue = []
@@ -224,10 +195,10 @@ def start(threadnum):
     i = 1
     while (i < threadnum):
         linki = userids[split_num * (i - 1):split_num * i]
-        linksqueue.append(wbrelation(linki))
+        linksqueue.append(NongGuanJia(linki))
         i = i + 1
     linkend = userids[split_num * (i - 1):links_len]
-    linksqueue.append(wbrelation(linkend))
+    linksqueue.append(NongGuanJia(linkend))
     i = 0
     while (i < threadnum):
         linksqueue[i].start()
