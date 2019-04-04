0517fdaf5db2139ddab2649bd7050591fa4e12ab
Yang <pccettfly@outlook.com>
2019-04-04 23:58:22 +0800

    update

--- Addkeyword.py
+++ Addkeyword.py
@@ -13,6 +13,7 @@ for ynzz in cursor:
     ynzzs.append(ynzz['keyword'])
 zbbhsets = list(set(zbbhs))
 ynzzssets = list(set(ynzzs))
+
 fatherkeywordsdict,fatherkeywordslist={},[]
 for keyword in zbbhsets:
     zbbhsetscopy=copy.deepcopy(zbbhsets)
@@ -28,15 +29,33 @@ for keyword in zbbhsets:
     for leftkeyword in zbbhsetscopy:
         if leftkeyword.find(keyword) != -1:
             fatherkeywordsdict[leftkeyword].append(keyword)
-print(fatherkeywordsdict)
+
+ynzzfatherkeywordsdict, ynzzfatherkeywordslist = {}, []
+for keyword in ynzzssets:
+    ynzzsetscopy = copy.deepcopy(ynzzssets)
+    ynzzsetscopy.remove(keyword)
+    for leftkeyword in ynzzsetscopy:
+        if leftkeyword.find(keyword) != -1:
+            ynzzfatherkeywordslist.append(leftkeyword)
+            ynzzfatherkeywordsdict[leftkeyword] = []
+ynzzfatherkeywordslist = list(set(ynzzfatherkeywordslist))
+for keyword in ynzzssets:
+    ynzzsetscopy = copy.deepcopy(ynzzssets)
+    ynzzsetscopy.remove(keyword)
+    for leftkeyword in ynzzsetscopy:
+        if leftkeyword.find(keyword) != -1:
+            ynzzfatherkeywordsdict[leftkeyword].append(keyword)
+
 def disasterjudge(content=''):
     localzbbh=copy.deepcopy(zbbhsets)
     foundkeyword=[]
-    #print(len(foundkeyword))
     for fatherkeyword in fatherkeywordslist:
         if content.find(fatherkeyword)!=-1:
-            foundkeyword.append(fatherkeyword)
-            localzbbh.remove(fatherkeyword)
+            foundkeyword.append({'keyword':fatherkeyword,'type':'zbbh','taged':True})
+            try:
+                localzbbh.remove(fatherkeyword)
+            except ValueError:
+                pass
             for childkeyword in fatherkeywordsdict[fatherkeyword]:
                 try:
                     localzbbh.remove(childkeyword)
@@ -45,11 +64,29 @@ def disasterjudge(content=''):
         else:pass
     for keyword in localzbbh:
         if content.find(keyword)!=-1:
-            foundkeyword.append(keyword)
+            foundkeyword.append({'keyword':keyword,'type':'zbbh','taged':True})
+
+
+    localynzz=copy.deepcopy(ynzzssets)
+    for fatherkeyword in ynzzfatherkeywordslist:
+        if content.find(fatherkeyword)!=-1:
+            foundkeyword.append({'keyword':fatherkeyword,'type':'ynzz','taged':True})
+            try:
+                localynzz.remove(fatherkeyword)
+            except ValueError:
+                pass
+            for childkeyword in ynzzfatherkeywordsdict[fatherkeyword]:
+                try:
+                    localynzz.remove(childkeyword)
+                except ValueError:
+                    pass
+        else:pass
+    for keyword in localynzz:
+        if content.find(keyword)!=-1:
+            foundkeyword.append({'keyword':keyword,'type':'ynzz','taged':True})
 
     return foundkeyword
 #disasterjudge()
-
 
 def addkeyword():
     pcoll = db['NongGuanJiaByProblem']
@@ -60,7 +97,6 @@ def addkeyword():
         else:
             if len(document['reply']) > 0:
                 for reply in document['reply']:
-                    # print(reply['content'])
                     foundkeyword=disasterjudge(reply['content'])
                     if len(foundkeyword)>0:
                         print(foundkeyword)
