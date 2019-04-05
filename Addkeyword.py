f0f7bcd18d8a049f5ebd73d1f683b666bdfd3c52
Yang <pccettfly@outlook.com>
2019-04-05 13:12:09 +0800

    update

--- Addkeyword.py
+++ Addkeyword.py
@@ -14,17 +14,17 @@ for ynzz in cursor:
 zbbhsets = list(set(zbbhs))
 ynzzssets = list(set(ynzzs))
 
-fatherkeywordsdict,fatherkeywordslist={},[]
+fatherkeywordsdict, fatherkeywordslist = {}, []
 for keyword in zbbhsets:
-    zbbhsetscopy=copy.deepcopy(zbbhsets)
+    zbbhsetscopy = copy.deepcopy(zbbhsets)
     zbbhsetscopy.remove(keyword)
     for leftkeyword in zbbhsetscopy:
         if leftkeyword.find(keyword) != -1:
             fatherkeywordslist.append(leftkeyword)
-            fatherkeywordsdict[leftkeyword]=[]
-fatherkeywordslist=list(set(fatherkeywordslist))
+            fatherkeywordsdict[leftkeyword] = []
+fatherkeywordslist = list(set(fatherkeywordslist))
 for keyword in zbbhsets:
-    zbbhsetscopy=copy.deepcopy(zbbhsets)
+    zbbhsetscopy = copy.deepcopy(zbbhsets)
     zbbhsetscopy.remove(keyword)
     for leftkeyword in zbbhsetscopy:
         if leftkeyword.find(keyword) != -1:
@@ -46,12 +46,17 @@ for keyword in ynzzssets:
         if leftkeyword.find(keyword) != -1:
             ynzzfatherkeywordsdict[leftkeyword].append(keyword)
 
+
 def disasterjudge(content=''):
-    localzbbh=copy.deepcopy(zbbhsets)
-    foundkeyword=[]
+    localzbbh = copy.deepcopy(zbbhsets)
+    foundkeyword = []
     for fatherkeyword in fatherkeywordslist:
-        if content.find(fatherkeyword)!=-1:
-            foundkeyword.append({'keyword':fatherkeyword,'type':'zbbh','taged':True})
+        if content.find(fatherkeyword) != -1:
+            foundkeyword.append({'keyword': fatherkeyword,
+                                 'type': 'zbbh',
+                                 'taged': True,
+                                 'keyvalue': None,
+                                 'link': None})
             try:
                 localzbbh.remove(fatherkeyword)
             except ValueError:
@@ -61,16 +66,24 @@ def disasterjudge(content=''):
                     localzbbh.remove(childkeyword)
                 except ValueError:
                     pass
-        else:pass
+        else:
+            pass
     for keyword in localzbbh:
-        if content.find(keyword)!=-1:
-            foundkeyword.append({'keyword':keyword,'type':'zbbh','taged':True})
+        if content.find(keyword) != -1:
+            foundkeyword.append({'keyword': keyword,
+                                 'type': 'zbbh',
+                                 'taged': True,
+                                 'keyvalue': None,
+                                 'link': None})
 
-
-    localynzz=copy.deepcopy(ynzzssets)
+    localynzz = copy.deepcopy(ynzzssets)
     for fatherkeyword in ynzzfatherkeywordslist:
-        if content.find(fatherkeyword)!=-1:
-            foundkeyword.append({'keyword':fatherkeyword,'type':'ynzz','taged':True})
+        if content.find(fatherkeyword) != -1:
+            foundkeyword.append({'keyword': fatherkeyword,
+                                 'type': 'ynzz',
+                                 'taged': True,
+                                 'keyvalue': None,
+                                 'link': None})
             try:
                 localynzz.remove(fatherkeyword)
             except ValueError:
@@ -80,24 +93,52 @@ def disasterjudge(content=''):
                     localynzz.remove(childkeyword)
                 except ValueError:
                     pass
-        else:pass
+        else:
+            pass
     for keyword in localynzz:
-        if content.find(keyword)!=-1:
-            foundkeyword.append({'keyword':keyword,'type':'ynzz','taged':True})
+        if content.find(keyword) != -1:
+            foundkeyword.append({'keyword': keyword,
+                                 'type': 'ynzz',
+                                 'taged': True,
+                                 'keyvalue': None,
+                                 'link': None})
 
     return foundkeyword
-#disasterjudge()
+# disasterjudge()
+
 
 def addkeyword():
     pcoll = db['NongGuanJiaByProblem']
     cursor = pcoll.find()
+    keywordNumMax = 0
     for document in cursor:
         if document['keywordNum'] > 0:
             continue
         else:
+            keywordNum = 0
             if len(document['reply']) > 0:
                 for reply in document['reply']:
-                    foundkeyword=disasterjudge(reply['content'])
-                    if len(foundkeyword)>0:
-                        print(foundkeyword)
-addkeyword()
+                    foundkeyword = disasterjudge(reply['content'])
+                    if len(foundkeyword) > 0:
+                        keywordNum += len(foundkeyword)
+                        reply['keyword'] = foundkeyword
+                if keywordNum > 0:
+                    if keywordNum > keywordNumMax:
+                        keywordNumMax = keywordNum
+                        print(keywordNumMax)
+                    document['keywordNum'] = keywordNum
+                    # print(document)
+                    pcoll.update_one(
+                        {"_id": document["_id"]},
+                        {
+                            "$set": {
+                                "keywordNum": document['keywordNum'],
+                                "reply": document['reply'],
+                                "taged": True
+                            }
+                        }
+
+                    )
+            else:
+                pass
+# addkeyword()
