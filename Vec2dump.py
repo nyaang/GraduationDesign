812c4850c1155bd2ac1902938c7594b1f8299929
Yang <pccettfly@outlook.com>
2019-05-09 21:18:54 +0800

    update

--- Vec2dump.py
+++ Vec2dump.py
@@ -1,9 +1,9 @@
 ﻿from pymongo import MongoClient
 client = MongoClient()
 db = client['NongGuanJia']
-ucoll=db['NongGuanJiaByUser']
-cursor = ucoll.find()
 def countnodesandvectors():
+    ucoll = db['NongGuanJiaByUser']
+    cursor = ucoll.find()
     uids,qids,nodes=[],[],[]
     for document in cursor:
         if len(document['Reply'])==0:
@@ -47,4 +47,131 @@ def dumpdatafile():
                 node2vecfile.write(str(reply['ruid'])+' '+str((document['qid']+140000))+'\n')
     hin2vecfile.close()
     node2vecfile.close()
-dumpdatafile()
+# dumpdatafile()
+
+def dataclean():
+    # hin2vecfile = open('Result/hin2vecdata_P.txt', 'r')
+    # u_pvecs = hin2vecfile.readlines()
+    # hin2vecfile.close()
+    # problem_user, problemlist = {}, []
+    # for i in range(1, len(u_pvecs)):
+    #     u_pvec = u_pvecs[i].split()
+    #     problemlist.append(u_pvec[2])
+    #
+    # problemlist = list(set(problemlist))
+    # for pid in problemlist:
+    #     problem_user[pid] = []
+    # for i in range(1, len(u_pvecs)):
+    #     u_pvec = u_pvecs[i].split()
+    #     problem_user[u_pvec[2]].append(u_pvec[0])
+
+    # lendict,lenlist={},[]
+    # for pid in problemlist:
+    #     lenlist.append(len(problem_user[pid]))
+    # lenlist=list(set(lenlist))
+    # lenlist.sort()
+    # for lenghth in lenlist:
+    #     lendict[lenghth]=0
+    # for pid in problemlist:
+    #     lendict[len(problem_user[pid])]+=1
+    # print(lendict)
+
+    # cleanproblemfile=open('hin2vecdata_P_cleaned.txt','w')
+    # problemlist.sort()
+    # left_problems=0
+    # for problemid in problemlist:
+    #     if len(problem_user[problemid])>5:
+    #         left_problems+=1
+    #         for userid in problem_user[problemid]:
+    #             cleanproblemfile.write(str(problemid) + '	Q	' + str(userid) + '	U	Q-U\n')
+    # print(left_problems)
+    # cleanproblemfile.close()
+
+
+    # cleanproblemfile=open('hin2vecdata_P_cleaned.txt','r')
+    # u_pvecs = cleanproblemfile.readlines()
+    # cleanproblemfile.close()
+    # user_problem, uidlist = {}, []
+    # for i in range(0, len(u_pvecs)):
+    #     u_pvec = u_pvecs[i].split()
+    #     uidlist.append(u_pvec[2])
+    #
+    # uidlist = list(set(uidlist))
+    # for uid in uidlist:
+    #     user_problem[uid] = []
+    # for i in range(0, len(u_pvecs)):
+    #     u_pvec = u_pvecs[i].split()
+    #     user_problem[u_pvec[2]].append(u_pvec[0])
+    #
+    # lendict,lenlist={},[]
+    # print(len(uidlist))
+    # for uid in uidlist:
+    #     lenlist.append(len(user_problem[uid]))
+    # lenlist=list(set(lenlist))
+    # lenlist.sort()
+    # for lenghth in lenlist:
+    #     lendict[lenghth]=0
+    # for uid in uidlist:
+    #     lendict[len(user_problem[uid])]+=1
+    # print(lendict)
+    #
+    # cleanuserfile=open('hin2vecdata_U_cleaned.txt','w')
+    # uidlist_copy=[]
+    # for uid in uidlist:
+    #     uidlist_copy.append(int(uid))
+    # uidlist_copy.sort()
+    # uidlist=[]
+    # for uid in uidlist_copy:
+    #     uidlist.append(str(uid))
+    # left_user,left_problem=0,[]
+    # for uid in uidlist:
+    #     if len(user_problem[uid])>5:
+    #         left_user+=1
+    #         for problemid in user_problem[uid]:
+    #             left_problem.append(problemid)
+    #             cleanuserfile.write(str(uid) + '	U	' + str(problemid) + '	Q	U-Q\n')
+    # left_problem=list(set(left_problem))
+    # print(left_user,len(left_problem))
+    # cleanuserfile.close()
+
+    hin2vecfile = open('hin2vecdata_U_cleaned.txt', 'r')
+    u_pvecs = hin2vecfile.readlines()
+    user_problem,uidlist={},[]
+    for i in range(1, len(u_pvecs)):
+        u_pvec = u_pvecs[i].split()
+        uidlist.append(u_pvec[0])
+    # print(len(u_pveclist))
+    uidlist = list(set(uidlist))
+    for uid in uidlist:
+        user_problem[uid] = []
+    for i in range(1, len(u_pvecs)):
+        u_pvec = u_pvecs[i].split()
+        user_problem[u_pvec[0]].append(u_pvec[2])
+    problems,vectornums=[],0
+    for uid in uidlist:
+        vectornums+=len(user_problem[uid])
+        for pid in user_problem[uid]:
+            problems.append(pid)
+    problems=list(set(problems))
+    print(len(uidlist),len(problems),vectornums)
+
+# dataclean()
+def user_problem():
+    f=open('Result/node_vectors.txt','r')
+    lines=f.readlines()
+    userfile=open('user_vectors.txt','w')
+    problemfile=open('problem_vectors.txt','w')
+    uservectors,problemvectors=[],[]
+    for i in range(1,len(lines)):
+        # print(lines[i])
+        vector=lines[i].split()
+        if int(vector[0])<140000:
+            uservectors.append(lines[i])
+        else:
+            problemvectors.append(lines[i])
+    userfile.writelines(uservectors)
+    problemfile.writelines(problemvectors)
+    userfile.close()
+    problemfile.close()
+    print(len(uservectors),len(problemvectors))
+    f.close()
